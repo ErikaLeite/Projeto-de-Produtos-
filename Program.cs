b6916556dcd67d238f19be4953dc7c835b379114
ERIKA LEITE PAIVA <you@example.com>
2021-01-08 21:05:31 -0300

    estrutura da classe Marca criada

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_de_Produtos_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ classes/Usuario.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Projeto_de_Produtos_.classes
+{
+    public class Usuario
+    {
+        int CodigoUser { get; set; }
+        string NomeUser { get; set; }
+        string Email { get; set; }
+        string Senha { get; set; }
+        DateTime DataCadastroUser { get; set; }
+        
+        
+        
+        
+    }
+}
